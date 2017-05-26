96223b898756c56937dcdcdc3e3d3df9bc186d66
Andrea Scocchi <scocchiello@gmail.com>
2017-05-26 16:52:47 +0200

    Paritissimi

---
+++ TeoGlass/TeoGlass/HomePageController.cs
@@ -0,0 +1,29 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace TeoGlass
+{
+    public partial class HomePageController : UIViewController
+    {
+        public HomePageController (IntPtr handle) : base (handle)
+        {
+            
+
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+            logoImage.Image = UIImage.FromFile("Images/inglassLogo.png");
+            logoImageIphone.Image = UIImage.FromFile("Images/inglassLogo.png");
+            Packing.BackgroundColor = UIColor.Blue;
+            Packing.SetTitleColor(UIColor.White, UIControlState.Normal);
+            Tests.BackgroundColor = UIColor.Blue;
+            Tests.SetTitleColor(UIColor.White, UIControlState.Normal);
+        }
+
+
+    }
+}
---
+++ TeoGlass/TeoGlass/HomePageController.designer.cs
@@ -0,0 +1,56 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace TeoGlass
+{
+    [Register ("HomePageController")]
+    partial class HomePageController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView logoImage { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView logoImageIphone { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Packing { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Tests { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (logoImage != null) {
+                logoImage.Dispose ();
+                logoImage = null;
+            }
+
+            if (logoImageIphone != null) {
+                logoImageIphone.Dispose ();
+                logoImageIphone = null;
+            }
+
+            if (Packing != null) {
+                Packing.Dispose ();
+                Packing = null;
+            }
+
+            if (Tests != null) {
+                Tests.Dispose ();
+                Tests = null;
+            }
+        }
+    }
+}
